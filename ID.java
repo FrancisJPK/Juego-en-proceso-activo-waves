13d75c6e8cf7e26e87224e7c6171908530cddf9c
FrnacisJP <albertoramiro2@gmail.com>
2017-04-12 03:00:40 -0300

    Delete ID.java

--- ID.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-//sirve para ordenar cosas por identificador osea yo puedo tener 2 objetos de juego
-//pero uno se va a identificar como enemigo y otra como jugador
-public enum ID {
-	Jugador(),
-	EnemigoBasico(),
-	Rastro();
-}
